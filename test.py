6c327fb4ebc37e1ecf6c63638f51ca53ae2f3455
Jonas Hyllengren <jonas.hyllengren@gmail.com>
2021-03-09 11:26:46 +0100

    update test script to give edge matrix instead of coordinates

--- test.py
+++ test.py
@@ -2,23 +2,27 @@ from anneal import SimAnneal
 import matplotlib.pyplot as plt
 import random
 
+def read_edges(path):
 
-def read_coords(path):
-    coords = []
+    edges = []
     with open(path, "r") as f:
         for line in f.readlines():
             line = [float(x.replace("\n", "")) for x in line.split(" ")]
-            coords.append(line)
-    return coords
-
+            edges.append(line)
+        N = len(edges)
+        # squarify
+        for i in range(0,N):
+            edges[i] = edges[i] + (N-i-1)*[0.0]
+            print(edges[i])
+    return edges
 
 def generate_random_coords(num_nodes):
     return [[random.uniform(-1000, 1000), random.uniform(-1000, 1000)] for i in range(num_nodes)]
 
 
 if __name__ == "__main__":
-    coords = read_coords("coord.txt")  # generate_random_coords(100)
-    sa = SimAnneal(coords, stopping_iter=5000)
+    edges = read_edges("edges.txt")  # generate_random_coords(100)
+    sa = SimAnneal(edges, stopping_iter=5000)
     sa.anneal()
     sa.visualize_routes()
     sa.plot_learning()
